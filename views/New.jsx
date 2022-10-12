dc3e3c40cb1ae8faaa3bbf9f74f1983697d863d0
Seezee23 <seezee146@yahoo.com>
2022-10-11 22:32:07 -0400

    first commit

---
+++ views/New.jsx
@@ -0,0 +1,11 @@
+const React = require('react')
+res.render('new')
+
+
+<form method="POST" action="/logs">
+    <input type="text" title="title"></input><br/>
+    <input type="textarea" title="entry"></input><br/>
+    <input type="checkbox" title="shipIsBroken"></input><br/>
+    <input type='submit'></input>
+
+</form>
